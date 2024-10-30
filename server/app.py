aee46ce480ae9d492c26c36c9a9538bb1f969407
Ronney Arias <ronneyarias@gmail.com>
2024-10-30 12:37:53 -0500

    complete lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,82 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods= ['GET', 'POST'])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+        records = []
+        for record in Message.query.order_by(Message.created_at.asc()).all():
+            record_dict = record.to_dict()
+            records.append(record_dict)
+
+        response = make_response(
+            records,
+            200
+        )
+
+        return response
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+
+        new_message = Message(
+            body=data.get("body"),
+            username=data.get("username"),
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        record_dict = new_message.to_dict()
+
+        response = make_response(
+            record_dict,
+            201
+        )
+
+        return response
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+
+    if message == None:
+        response_body = {
+            "message": "This message dos not exist in tour database. Please try again."
+        }
+        response = make_response(response_body, 404)
+
+        return response
+
+    if request.method == 'GET':
+        message_dict = message.to_dict()
+
+        response = make_response(
+            message_dict,
+            200
+        )
+
+        return response
+    
+    elif request.method == 'PATCH':
+        data = request.get_json()
+        if not data:
+            return make_response({"message": "No data provided for update."}, 400)
+
+        # Update attributes
+        for attr, value in data.items():
+            if hasattr(message, attr):
+                setattr(message, attr, value)
+
+        db.session.commit()
+
+        return make_response(message.to_dict(), 200)
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return make_response({"message": "Message deleted successfully."}, 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
